8e0503272e8aa40e5cda5aaa1b0b3f768e9b2af5
pekchan30 <pekchan@live.com.mx>
2013-05-25 22:59:34 -0500

    El script itera de forma recursiva el directorio

--- imagemagick-bash/convertir.sh
+++ imagemagick-bash/convertir.sh
@@ -2,18 +2,31 @@
 #Tomamos dir raiz y dir destino
 DIR=$1;
 DEST=$2;
-
+#Accedemos al dir raiz
+cd $DIR;
 #Metodo con un for que convierte y copia las imagenes
 convertir(){
-	#Accedemos al dir raiz
-	cd $DIR;
 	#Para que el listado de archivos sea archivo por linea
 	dir=$(dir -1);
-	#Convertimos las imagenes
+	#Iteramos el directorio
 	for file in $dir;do
-		#Convertimos con la herramienta convert de imageMagick
-		echo "Convirtiendo: "$file;
-		convert $file -quality 50 $DEST$file;
+		#Cachamos la variable que mandaremos si encuentra un directorio (Para hacerlo recursivo)
+		tem=$1;
+		#Si es un diretorio lo crea
+		if [ -d $file ]; then
+			echo "Creando directorio: "$DEST$tem"/"$file;
+			mkdir $DEST$tem"/"$file;
+			#ingresa a el y llama nuevamente este mismo metodo
+			cd $file;
+			#Se manda a si mismo el dir temporal (padre) y el dir actual concatenados
+			convertir $tem"/"$file;
+			#sale del directorio actual y sigue iterando el directorio principal
+			cd ..;
+		else
+			#Si es un archivo Convertimos con la herramienta convert de imageMagick
+			echo "Convirtiendo: "$DEST$tem"/"$file;
+			convert $file -quality 50 $DEST$tem"/"$file;
+		fi
 	done;
 }
 
